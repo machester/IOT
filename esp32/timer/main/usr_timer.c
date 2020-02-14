2fe34e5265b339bfead1d46189e9978f55ebe94a
machester <shusheng1991@gmail.com>
2020-02-14 18:03:13 +0800

    esp32 timer pass

--- esp32/timer/main/usr_timer.c
+++ esp32/timer/main/usr_timer.c
@@ -6,88 +6,89 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include "usr_timer.h"
-// #include "sdkconfig.h"
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_types.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "soc/timer_group_struct.h"
+#include "driver/timer.h"
+#include "esp_timer.h"
+#include "driver/periph_ctrl.h"
+
 
 /* Can run 'make menuconfig' to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO_Index 		2
-	
-#define BLINK_TASK_NAME				"led blink"
-#define TICK_COUNTER_TASK_NAME		"tick counter"
-#define TIMER_NAME					"USR_TIMER"
+#define BLINK_GPIO 		CONFIG_BLINK_GPIO
 
-void blink_task(void * param);
-void tick_counter_task(void * param);	
+const char timer_name[] = "fw_timer"; 
+uint8_t args_transfer[] = "timer args transfer";
 
-static unsigned int usr_ticks;
+esp_timer_handle_t fw_timer_handler = 0;
+
+static uint32_t recall_cnt;
+
+void fw_timer_callback(void *args)
+{
+	uint8_t * param_transfer_in = (uint8_t *)args;
+	printf("Transfer in param is: %s\n", param_transfer_in);
+
+	// 获取时间戳
+	int64_t ticks = esp_timer_get_time();
+	printf("---> timer cnt = %lld \n", ticks);
+
+	if(ticks > (300 * 1000 * 1000)) { // stop counter afer 50 seconds
+		printf("timer stop protocal\n");
+		esp_timer_stop(fw_timer_handler);
+		esp_timer_delete(fw_timer_handler);
+		
+		// reboot system
+		esp_restart();
+	}
+	// blink led
+	gpio_set_level(BLINK_GPIO, (recall_cnt % 2));
+	recall_cnt++;
+}
 
 
-	typedef struct 
-	{
-		esp_timer_cb_t callback;		//!< Function to call when timer expires
-		void* arg;						//!< Argument to pass to the callback
-		esp_timer_dispatch_t dispatch_method;	//!< Call the callback from task or from ISR
-		const char* name;				//!< Timer name, used in esp_timer_dump function
-	} esp_timer_create_args_t;
 
 void app_main()
 {
 	vTaskDelay(pdMS_TO_TICKS(100));			//等待系统初始化
 	
-	gpio_pad_select_gpio(BLINK_GPIO_Index);
+	gpio_pad_select_gpio(BLINK_GPIO);
 	/* Set the GPIO as a push/pull output */
-	gpio_pad_pullup(BLINK_GPIO_Index);
-	gpio_set_direction(BLINK_GPIO_Index, GPIO_MODE_OUTPUT);
+	gpio_pad_pullup(BLINK_GPIO);
+	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
+	/** start: create timer ------------------------------*/
 
-	esp_timer_create_args_t tim = {
-		.callback = tim_recall,
-		.arg = NULL,
-		.name = TIMER_NAME,
+	esp_timer_create_args_t timer_fill = {
+		.callback = &fw_timer_callback,
+		// .arg = NULL;
+		.arg = (void *)args_transfer,
+		.name = timer_name				// timer name
 	};
+
+	esp_err_t err = esp_timer_create(&timer_fill, &fw_timer_handler);
+	if(err != ESP_OK) {
+		printf("timer create failed \n");
+		return;
+	}
+	err = esp_timer_start_periodic(fw_timer_handler, (0.5 * (1000 * 1000))); // recall every sec
+	if(err != ESP_OK) {
+		printf("timer start period failed \n");
+		return;
+	} else {
+		printf("timer create succeed, timer name: %s.\n", timer_name);
+	}
 	
-	// 任务栈的大小 小于 1024 报错, 制定cpu创建task
-	// configMINIMAL_STACK_SIZE
-	xTaskCreatePinnedToCore(blink_task, BLINK_TASK_NAME, 2048, "blink_task", 5, NULL, tskNO_AFFINITY);
-	xTaskCreate(tick_counter_task, TICK_COUNTER_TASK_NAME, 2048, TICK_COUNTER_TASK_NAME, 6, NULL);
-}
+	/** end: create timer ------------------------------*/
 
-void blink_task(void * param)
-{
-	timer_init(timer_group_t group_num, timer_idx_t timer_num, const timer_config_t * config)
-		esp_timer_create
-		esp_timer_start_periodic
 
-	while(1) {
-		printf("LED loop, In task: %s,\n", (char *)param);	
-		gpio_set_level(BLINK_GPIO_Index, 1);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-		gpio_set_level(BLINK_GPIO_Index, 0);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-		gpio_set_level(BLINK_GPIO_Index, 1);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-		gpio_set_level(BLINK_GPIO_Index, 0);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-		gpio_set_level(BLINK_GPIO_Index, 1);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
-
-		gpio_set_level(BLINK_GPIO_Index, 0);
-		vTaskDelay(1500 / portTICK_PERIOD_MS);
-	}
-
-}
-void tick_counter_task(void * param)
-{
-	while(1) {
-		printf("Ticks loop, In task: %s,\n", (char *)param);	
-		gpio_set_level(BLINK_GPIO_Index, 1);
-		vTaskDelay(200 / portTICK_PERIOD_MS);
-		gpio_set_level(BLINK_GPIO_Index, 0);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		printf("Ticks: %d\n", usr_ticks);
-		usr_ticks++;
-	}
 }
 
--- esp32/timer/main/usr_timer.h
+++ esp32/timer/main/usr_timer.h
@@ -1,17 +1,6 @@
 #ifndef __USR_TIMER_H
 #define __USR_TIMER_H
 
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "esp_types.h"
-#include "driver/gpio.h"
-#include "esp_log.h"
-#include "soc/timer_group_struct.h"
-#include "driver/timer.h"
-#include "esp_timer.h"
-
 #define DEBUG               1
 
 
